36107007c779da80f152323c41c848d1c5d28982
Cainie <karol.marszalik@gmail.com>
2020-06-02 23:19:33 +0200

    sceneInteractions: moved elevator logic to elevator manager

--- Assets/Scripts/Environment/InteractibleButton.cs
+++ Assets/Scripts/Environment/InteractibleButton.cs
@@ -1,33 +1,18 @@
 ﻿using System.Collections;
 using UnityEngine;
-using ElevatorSettings;
 
 namespace Environment{
     public class InteractibleButton : MonoBehaviour{
         [SerializeField] private Transform targetTransform;
-        private ElevatorSettings.ElevatorSettings _elevatorSettings;
+        private ElevatorSettings.ElevatorManager _elevatorManager;
 
-        public ElevatorSettings.ElevatorSettings ElevatorSettings{
-            set => _elevatorSettings = value;
+        public ElevatorSettings.ElevatorManager ElevatorManager{
+            set => _elevatorManager = value;
         }
 
         public virtual void Interact(){
-            if (_elevatorSettings.ElevatorIsMoving){ return; }
-            StartCoroutine(MoveElevatorToTargetTransform());
-        }
-        
-        
-        protected IEnumerator MoveElevatorToTargetTransform(){
-            _elevatorSettings.ElevatorIsMoving = true;
-            var targetPosition = targetTransform.position;
-            var elevatorPosition = _elevatorSettings.ElevatorRigidbody.transform.position;
-            while (elevatorPosition != targetPosition){
-                elevatorPosition = _elevatorSettings.ElevatorRigidbody.transform.position;
-                _elevatorSettings.ElevatorRigidbody.MovePosition(Vector3.MoveTowards(elevatorPosition,targetPosition,
-                    Time.fixedDeltaTime * _elevatorSettings.ElevatorSpeed));
-                yield return null;
-            }
-            _elevatorSettings.ElevatorIsMoving = false;
+            if (_elevatorManager.ElevatorIsMoving){ return; }
+            StartCoroutine(_elevatorManager.MoveElevatorToTargetTransform(targetTransform));
         }
     }
 }
