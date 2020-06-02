bdafe5df2c1eda68ef748f66f9ad2878810d8d7e
Cainie <karol.marszalik@gmail.com>
2020-06-02 20:10:09 +0200

    sceneInteractions: player scripts fixes

--- Assets/Scripts/Player/PlayerLookAt.cs
+++ Assets/Scripts/Player/PlayerLookAt.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerLookAt : MonoBehaviour{
-    [SerializeField] private float mouseSensitivity;
-    [SerializeField] private Transform playerTransform;
+namespace Player{
+    public class PlayerLookAt : MonoBehaviour{
+        [SerializeField] private float mouseSensitivity;
+        [SerializeField] private Transform playerTransform;
 
-    private float _xRotation;
+        private float _xRotation;
 
-    private void Start(){
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+        private void Start(){
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
-    private void Update(){
-        var mouseXAxis = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        var mouseYAxis = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        private void Update(){
+            var mouseXAxis = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            var mouseYAxis = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        _xRotation -= mouseYAxis;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+            _xRotation -= mouseYAxis;
+            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
 
 
-        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
-        playerTransform.Rotate(Vector3.up * mouseXAxis);
+            transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+            playerTransform.Rotate(Vector3.up * mouseXAxis);
+        }
     }
 }
